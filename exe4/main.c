3e9fc7eb9c4d057558186d639b02c0e5e9c4b98c
Felipe <fepodqueiroz@gmail.com>
2025-03-17 16:10:54 -0300

    EX4.1

--- exe4/main.c
+++ exe4/main.c
@@ -19,14 +19,12 @@ SemaphoreHandle_t xSemaphore_r;
 SemaphoreHandle_t xSemaphore_g;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_r, 0);
-    }
-}
-
-void btn_callback2(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        xSemaphoreGiveFromISR(xSemaphore_g, 0);
+    if (events == GPIO_IRQ_EDGE_FALL) { 
+        if (gpio == BTN_PIN_R) {
+            xSemaphoreGiveFromISR(xSemaphore_r, 0);
+        } else if (gpio == BTN_PIN_G) {
+            xSemaphoreGiveFromISR(xSemaphore_g, 0);
+        }
     }
 }
 
@@ -96,7 +94,7 @@ void btn_2_task(void *p) {
     gpio_set_dir(BTN_PIN_G, GPIO_IN);
     gpio_pull_up(BTN_PIN_G);
     gpio_set_irq_enabled_with_callback(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true,
-                                       &btn_callback2);
+                                       &btn_callback);
 
     int delay = 0;
     while (true) {
